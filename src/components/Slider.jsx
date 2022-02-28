5aa089dd2f6d4fd1018ed45387dce418145b96d0
Dev-Elie <eochieng9448@gmail.com>
2022-02-28 20:12:50 +0300

    Added Product Sliders

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,16 +1,59 @@
 import styled from "styled-components";
-// import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
-// import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
-
-import KeyboardArrowRightOutlinedIcon from '@mui/icons-material/KeyboardArrowRightOutlined';
-import KeyboardArrowLeftOutlinedIcon from '@mui/icons-material/KeyboardArrowLeftOutlined';
+import KeyboardArrowRightOutlinedIcon from "@mui/icons-material/KeyboardArrowRightOutlined";
+import KeyboardArrowLeftOutlinedIcon from "@mui/icons-material/KeyboardArrowLeftOutlined";
+import { useState } from "react";
+import sliderItems from "../data";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: tomato;
   position: relative;
+  overflow: hidden;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${props => props.slideIndex * -100}vw);
+`;
+
+const Slide = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #${(props) => props.bg};
+`;
+const ImgContainer = styled.div`
+  flex: 1;
+  height: 100%;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Desc = styled.p`
+  margin: 50px 0;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
 `;
 
 const Arrow = styled.div`
@@ -25,19 +68,42 @@ const Arrow = styled.div`
   position: absolute;
   top: 0;
   bottom: 0;
-  left: ${props=> props.direction === "left" && "10px"};
-  right: ${props=> props.direction === "right" && "10px"};
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
   margin: auto;
   opacity: 0.5;
+  z-index: 2;
 `;
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={() => handleClick("left")}>
         <KeyboardArrowLeftOutlinedIcon />
       </Arrow>
-      <Arrow direction="right">
+      <Wrapper slideIndex={slideIndex} >
+        {sliderItems.map((item) => (
+          <Slide bg={item.bg}>
+            <ImgContainer>
+              <Image src={item.img} />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Desc>{item.desc}</Desc>
+              <Button>SHOP NOW</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
         <KeyboardArrowRightOutlinedIcon />
       </Arrow>
     </Container>
---
+++ src/data.js
@@ -0,0 +1,25 @@
+const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/jr891Ky/pexels-tunde-visuals-9895814.jpg",
+    title: "SUMMER SALE",
+    desc: "SUMMER PINK AND GRAY LADIES OUTFIT, ALL SIZES",
+    bg: "fcf1ed",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/jr891Ky/pexels-tunde-visuals-9895814.jpg",
+    title: "POPULAR SALE",
+    desc: "SUMMER PINK AND GRAY LADIES OUTFIT, ALL SIZES",
+    bg: "fbf0f4",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/jr891Ky/pexels-tunde-visuals-9895814.jpg",
+    title: "OUTDOOR SALE",
+    desc: "SUMMER PINK AND GRAY LADIES OUTFIT, ALL SIZES",
+    bg: "f5fafd",
+  },
+];
+
+export default sliderItems
